3a78b219c4d5dc7e71a10ff9f4eab33d65619bcc
Sushmithamallesh <sushdec6@gmail.com>
2024-11-11 13:44:45 +0530

    add field notes backup

--- expose_bear_note.sh
+++ expose_bear_note.sh
@@ -4,17 +4,20 @@
 BEAR_NOTES_DIR=~/Documents/projects/bear-notes-backup/Bear
 HUGO_BLOG_DIR=~/Documents/projects/sushispot
 
-# Set the filename of the Markdown file you want to expose
+# Set the filename of the Markdown files you want to expose
 MARKDOWN_FILE_ML="Notes on ML.md"
 MARKDOWN_FILE_DISTRIBUTED_SYSTEM="Notes on Distributed Systems.md"
+MARKDOWN_FILE_ORDINARY="Field Notes on the Ordinary.md"
 
 ASSETS_DIR="BearImages"
 HUGO_FILE_NAME_LLM=content/notes/runningnoteml.md
 HUGO_FILE_NAME_DISTRIBUTED_SYSTEM=content/notes/runningnotedistributedsystems.md
+HUGO_FILE_NAME_ORDINARY=content/notes/fieldnotesontheordinary.md
 
-# Copy the Markdown file from Bear notes backup to Hugo blog content directory
+# Copy the Markdown files from Bear notes backup to Hugo blog content directory
 cp "$BEAR_NOTES_DIR/$MARKDOWN_FILE_ML" "$HUGO_BLOG_DIR/$HUGO_FILE_NAME_LLM"
 cp "$BEAR_NOTES_DIR/$MARKDOWN_FILE_DISTRIBUTED_SYSTEM" "$HUGO_BLOG_DIR/$HUGO_FILE_NAME_DISTRIBUTED_SYSTEM"
+cp "$BEAR_NOTES_DIR/$MARKDOWN_FILE_ORDINARY" "$HUGO_BLOG_DIR/$HUGO_FILE_NAME_ORDINARY"
 
 # Copy the BearImages directory recursively from Bear notes backup to Hugo blog directory
 cp -r "$BEAR_NOTES_DIR/BearImages" "$HUGO_BLOG_DIR/static/"
